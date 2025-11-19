a2d9751d451a515f567f531c962d9f3b5bfcfd19
Muhammad Farrel fastha_114 <farrelfastha11@gmail.com>
2025-11-19 17:48:53 +0700

    tambah file

---
+++ beranda.php
@@ -0,0 +1,17 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "web_review");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <label for=""></label>
+    <input type="text">
+</body>
+</html>
