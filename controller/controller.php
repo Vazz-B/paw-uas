bcfa1ecbb33e790cb3972b32892deb203631892c
Vazz-B <excuteslight@gmail.com>
2025-11-25 15:58:42 +0700

    perubahan

--- controller/controller.php
+++ controller/controller.php
@@ -1,39 +1,6 @@
 <?php
-require_once "./Model/user.php";
+require_once __DIR__ . '/../model/user.php';
 
-$action = $_GET['action'] ?? 'list';
+$users = getAllUser();
 
-// Tampilkan daftar user
-if ($action == 'list') {
-    $data = getAll();
-    include "./View/list.php";
-}
-
-// Form tambah
-else if ($action == 'add') {
-    include "./View/form.php";
-}
-
-// Simpan data baru
-else if ($action == 'store') {
-    insertData($_POST['nama'], $_POST['email'], $_POST['password'], $_POST['role'], $_POST['sekolah_id']);
-    header("Location: ./index.php");
-}
-
-// Form edit
-else if ($action == 'edit') {
-    $row = getById($_GET['user_id']);
-    include "./view/form.php";
-}
-
-// Update
-else if ($action == 'update') {
-    updateData($_POST['user_id'], $_POST['nama'], $_POST['email'], $_POST['password'], $_POST['role'], $_POST['sekolah_id']);
-    header("Location: index.php");
-}
-
-// Hapus
-else if ($action == 'delete') {
-    deleteData($_GET['user_id']);
-    header("Location: ./index.php");
-}
+require_once __DIR__ . '/../view/user.php';
--- model/user.php
+++ model/user.php
@@ -1,39 +1,41 @@
 <?php
-require_once "./koneksi.php";
+require_once __DIR__ . '/../koneksi.php';
 
-function getAll() {
+function getAllUser() {
     global $conn;
-    return mysqli_query($conn, "SELECT * FROM user ORDER BY user_id DESC");
+    $query = mysqli_query($conn, "SELECT * FROM user");
+    return $query;
 }
 
-function getById($id) {
-    global $conn;
-    $q = mysqli_query($conn, "SELECT * FROM user WHERE user_id = $id");
-    return mysqli_fetch_assoc($q);
-}
+// function getById($id) {
+//     global $conn;
+//     $q = mysqli_query($conn, "SELECT * FROM user WHERE user_id = $id");
+//     return mysqli_fetch_assoc($q);
+// }
 
-function insertData($nama, $email, $password, $role, $sekolah_id) {
-    global $conn;
-    return mysqli_query($conn, "
-        INSERT INTO user(nama, email, password, role, sekolah_id)
-        VALUES('$nama', '$email', '$password', '$role', '$sekolah_id')
-    ");
-}
+// function insertData($nama, $email, $password, $role, $sekolah_id) {
+//     global $conn;
+//     return mysqli_query($conn, "
+//         INSERT INTO user(nama, email, password, role, sekolah_id)
+//         VALUES('$nama', '$email', '$password', '$role', '$sekolah_id')
+//     ");
+// }
 
-function updateData($id, $nama, $email, $password, $role, $sekolah_id) {
-    global $conn;
-    return mysqli_query($conn, "
-        UPDATE user SET 
-            nama='$nama',
-            email='$email',
-            password='$password',
-            role='$role',
-            sekolah_id='$sekolah_id'
-        WHERE user_id=$id
-    ");
-}
+// function updateData($id, $nama, $email, $password, $role, $sekolah_id) {
+//     global $conn;
+//     return mysqli_query($conn, "
+//         UPDATE user SET 
+//             nama='$nama',
+//             email='$email',
+//             password='$password',
+//             role='$role',
+//             sekolah_id='$sekolah_id'
+//         WHERE user_id=$id
+//     ");
+// }
 
-function deleteData($id) {
-    global $conn;
-    return mysqli_query($conn, "DELETE FROM user WHERE user_id = $id");
-}
+// function deleteData($id) {
+//     global $conn;
+//     return mysqli_query($conn, "DELETE FROM user WHERE user_id = $id");
+// }
+?>
