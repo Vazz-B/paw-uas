82722eaa2f4a7b823e96a840243cab99db737926
Bungaadenis <bungaadns@gmail.com>
2025-12-08 14:05:28 +0700

    baru

--- controller/dashboardController.php
+++ controller/dashboardController.php
@@ -15,3 +15,31 @@ function tampilDashboard() {
     // Kirim ke view
     require_once __DIR__ . '/../view/user/dashboard_user.php';
 }
+
+function tampilGame() {
+    session_start();
+
+    if (!isset($_SESSION['login'])) {
+        header("Location: index.php?action=login");
+        exit;
+    }
+
+    // kategori Game = 2
+    $posts = ambilPostinganByKategori(2);
+
+    require_once __DIR__ . '/../view/user/game.php';
+}
+
+function tampilLagu() {
+    session_start();
+
+    if (!isset($_SESSION['login'])) {
+        header("Location: index.php?action=login");
+        exit;
+    }
+
+    // kategori Lagu = 3
+    $posts = ambilPostinganByKategori(3);
+
+    require_once __DIR__ . '/../view/user/lagu.php';
+}
--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once 'controller/daftarController.php';
 require_once 'controller/dashboardController.php';
 require_once 'controller/tambahPostinganController.php';
 
+
 $action = $_GET['action'] ?? 'login';
 
 if ($action == "login") tampilLogin();
@@ -15,3 +16,11 @@ elseif ($action == "proses_daftar") prosesDaftar();
 elseif ($action == "cari_sekolah") cariSekolahAjax();
 elseif ($action == "dashboard") tampilDashboard();
 elseif ($action == "simpan_post") prosesSimpanPost();
+if (isset($_GET['action']) && $_GET['action'] === 'game') {
+    tampilGame();
+    exit;
+}
+if (isset($_GET['action']) && $_GET['action'] === 'lagu') {
+    tampilLagu();
+    exit;
+}
